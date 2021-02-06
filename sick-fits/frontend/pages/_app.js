4523527e0b8e661f54d241378693ea9bdd8dafba
Andrew <andrew@bbm.co.uk>
2021-02-06 07:26:40 +0000

    Add App

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,9 +1,8 @@
-import Page from "../components/Page";
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Page>
+    <>
+      <h1>App Level</h1>
       <Component {...pageProps} />
-    </Page>
+    </>
   );
 }
