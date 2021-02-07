69025580dfa4fcecec8742738c70b123440ea4b2
Andrew <andrew@bbm.co.uk>
2021-02-07 08:30:16 +0000

    Visualising Route Changes - nProgress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,9 +1,15 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
-      <h1>App Level</h1>
       <Component {...pageProps} />
     </Page>
   );
