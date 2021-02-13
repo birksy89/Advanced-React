88ca31ddd1b90a4ffca2bcf33d837e7f60b08939
Andrew <andrew@bbm.co.uk>
2021-02-13 15:49:54 +0000

    45 - Cart :fire: - Using React Context for our Cart State

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { useCart } from '../lib/cartState';
 import SignOut from './SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
 export default function Nav() {
   const user = useUser();
+  const { openCart } = useCart();
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
@@ -13,9 +15,17 @@ export default function Nav() {
           <Link href="/sell">Sell</Link>
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
+          <button type="button" onClick={openCart}>
+            My Cart
+          </button>
         </>
       )}
-      {!user ? <Link href="/signin">Sign In</Link> : <SignOut />}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
