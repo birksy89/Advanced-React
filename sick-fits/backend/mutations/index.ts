f331fc7657acb60a63e319c29dc35d82d74922a6
Andrew <andrew@bbm.co.uk>
2021-02-28 11:05:59 +0000

    Fix error

--- sick-fits/backend/mutations/index.ts
+++ sick-fits/backend/mutations/index.ts
@@ -8,7 +8,7 @@ export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs: graphql`
     type Mutation {
       addToCart(productId: ID): CartItem
-      checkout(token String!): Order
+      checkout(token: String!): Order
     }
   `,
   resolvers: {
